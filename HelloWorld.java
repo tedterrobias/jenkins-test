98db83aade842bd082cf1df4ad9051e9163b470f
tedterrobias <61285885+tedterrobias@users.noreply.github.com>
2021-05-18 18:36:38 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String [] args) {
 		System.out.println("Hello World!");
-		System.out("Made Some Changes");
+		System.out.println("Made Some Changes");
 	}
 }
